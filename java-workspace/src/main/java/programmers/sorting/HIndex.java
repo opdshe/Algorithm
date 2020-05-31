4c207526dbb0cb14e927d5ed5089649892546570
opdshe <50160282+opdshe@users.noreply.github.com>
2020-05-31 23:03:20 +0900

    Add : Programmers - sorting - H-Index

--- java-workspace/src/main/java/programmers/sorting/HIndex.java
+++ java-workspace/src/main/java/programmers/sorting/HIndex.java
@@ -1,7 +1,28 @@
 package programmers.sorting;
 
+import java.util.Arrays;
+
 public class HIndex {
     public static void main(String[] args) {
+        solution(new int[]{44,22});
+    }
 
+    public static int solution(int[] citations) {
+        int min = 0;
+        int maxCount = citations.length;
+        int maxH = min;
+        for (int h = 0; h <= maxCount; h++) {
+            int finalH = h;
+            int overThanH = (int) Arrays.stream(citations)
+                    .filter(integer->integer >= finalH)
+                    .count();
+            if (overThanH >= finalH) {
+                maxH = finalH;
+                continue;
+            }
+            break;
+        }
+        System.out.println(maxH);
+        return maxH;
     }
 }
