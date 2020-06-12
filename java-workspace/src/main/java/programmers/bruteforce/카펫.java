9d4148a2332d944c1702044ba9b6a16d69befb27
opdshe <opdshe@naver.com>
2020-06-12 13:30:37 +0900

    Add : Programmers - bruteforce - 카펫

---
+++ java-workspace/src/main/java/programmers/bruteforce/카펫.java
@@ -0,0 +1,26 @@
+package main.java.programmers.bruteforce;
+
+public class 카펫 {
+    public static void main(String[] args) {
+        solution(10, 2);
+    }
+
+    public static int[] solution(int brown, int yellow) {
+        int[] answer = null;
+        for (int width = 3; width <= 5000; width++) {
+            for (int height = 3; height <= width; height++) {
+                int currentBrown = (width * 2) + 2 * (height - 2);
+                if (currentBrown == brown) {
+                    int currentYellow = (width - 2) * (height - 2);
+                    if (currentYellow == yellow) {
+                        System.out.println("width =" + width + ", height = " +height);
+                        answer = new int[]{width, height};
+                        break;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
+
