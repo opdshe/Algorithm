ed68abc7ee978849ff515ba1218bec6c514abafa
opdshe <opdshe@naver.com>
2020-07-28 21:57:52 +0900

    Add : ATM 추가

--- java-workspace2/src/백준/숨바꼭질.java
+++ java-workspace2/src/백준/숨바꼭질.java
@@ -8,35 +8,6 @@ public class 숨바꼭질 {
         int[] input = Arrays.stream(scanner.nextLine().split(" "))
                 .mapToInt(Integer::parseInt)
                 .toArray();
-        int subinPosition = input[0];
-        int dongsangPosition = input[1];
-        bfs(subinPosition, dongsangPosition);
-    }
-    private static void bfs(int subinPosition, int dongsangPosition) {
-        int[] visited = new int[100000];
-        visited[subinPosition] = 0;
-        Queue<Integer> queue = new ArrayDeque<>();
-        queue.add(subinPosition);
-        while (!queue.isEmpty()) {
-            Integer currentPosition = queue.poll();
-            if (currentPosition == dongsangPosition) {
-                break;
-            }
-            List<Integer> subinNextPositions = getSubinNextPositions(currentPosition);
-            for (Integer subinNextPosition : subinNextPositions) {
-                if (visited[subinNextPosition] == 0 && subinNextPosition<= 100000 && subinNextPosition>=0) {
-                    visited[subinNextPosition] = visited[currentPosition] + 1;
-                    queue.add(subinNextPosition);
-                }
-            }
-        }
-        System.out.println(visited[dongsangPosition]);
-    }
-    private static List<Integer> getSubinNextPositions (int currentPosition) {
-        List<Integer> nextPositions = new ArrayList<>();
-        nextPositions.add(currentPosition-1);
-        nextPositions.add(currentPosition+1);
-        nextPositions.add(currentPosition*2);
-        return nextPositions;
+        System.out.println(input[0] + input[1]);
     }
 }
