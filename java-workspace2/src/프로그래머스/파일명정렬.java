cdd0fca2bc48c52dc6e2b12aad330e6f700b0955
opdshe <opdshe@naver.com>
2020-08-05 00:17:33 +0900

    Add : 파일명정렬 수정

--- java-workspace2/src/프로그래머스/파일명정렬.java
+++ java-workspace2/src/프로그래머스/파일명정렬.java
@@ -1,14 +1,15 @@
 package 프로그래머스;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class 파일명정렬 {
     public static void main(String[] args) {
-        solution(new String[]{"img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG" });
+        //"img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"
+        //"F-5 Freedom Fighter", "B-50 Superfortress", "A-10 Thunderbolt II", "F-14 Tomcat"
+        solution(new String[]{"F-5 Freedom Fighter", "B-50 Superfortress", "A-10 Thunderbolt II", "F-14 Tomcat"});
+        System.out.println("F-".compareToIgnoreCase("A-"));
     }
 
     public static String[] solution(String[] files) {
@@ -18,10 +19,36 @@ public class 파일명정렬 {
             fileList.add(file);
         }
 
+        fileList.sort((File a, File b) -> {
+            if (a.head.compareToIgnoreCase(b.head) >= 1) {
+                //System.out.println("comparing head, " + a.toString() + " > " + b.toString());
+                return 1;
+            } else if (a.head.compareToIgnoreCase(b.head) <= -1) {
+                //System.out.println("comparing head, " + a.toString() + " < " + b.toString());
+                return -1;
+            }
+            if (Integer.parseInt(a.number) > Integer.parseInt(b.number)) {
+                //System.out.println("comparing number, " + a.toString() + " > " + b.toString());
+                return 1;
+            } else if (Integer.parseInt(a.number) < Integer.parseInt(b.number)) {
+                //System.out.println("comparing head, " + a.toString() + " > " + b.toString());
+                return -1;
+            }
+            if (a.idx > b.idx) {
+                return 1;
+            }
+            return -1;
+        });
+
         for (File file : fileList) {
-            System.out.println(file);
+            System.out.println("head: " + file.head +", number: " +file.number +", tail: " + file.tail );
         }
-        return null;
+        String[] result = fileList.stream()
+                .map(File::toString)
+                .collect(Collectors.toList())
+                .toArray(new String[0]);
+
+        return result;
     }
 
     private static File makeFile(String file, int idx) {
@@ -40,9 +67,6 @@ public class 파일명정렬 {
                 break;
             }
         }
-        System.out.println("head: " + head.toString());
-        System.out.println("number: " + number.toString());
-        System.out.println("tail: " + tail);
         return new File(head.toString(), number.toString(), tail, idx);
     }
 
@@ -59,7 +83,6 @@ public class 파일명정렬 {
             this.idx = idx;
         }
 
-        @Override
         public String toString() {
             return head + number + tail;
         }
