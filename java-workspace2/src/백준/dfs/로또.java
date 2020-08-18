e0611860f64baf6a2301f986ffb69deccdf241a9
opdshe <opdshe@naver.com>
2020-08-18 22:40:16 +0900

    Add : 로또

--- java-workspace2/src/백준/dfs/로또.java
+++ java-workspace2/src/백준/dfs/로또.java
@@ -1,9 +1,47 @@
 package 백준.dfs;
 
-public class 로또 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class 로또 {
+    static boolean[] visited;
+    static int countOfNumbers;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            countOfNumbers = scanner.nextInt();
+            if (countOfNumbers == 0) {
+                return;
+            }
+
+            visited = new boolean[countOfNumbers];
+            String[] lotto = new String[countOfNumbers];
+
+            for (int i = 0; i < countOfNumbers; i++) {
+                lotto[i] = String.valueOf(scanner.nextInt());
+            }
+
+            dfs(lotto, 0, 0);
+            System.out.println();
+        }
     }
 
-
+    private static void dfs(String[] lotto, int idx, int count) {
+        if (count == 6) {
+            StringBuilder answer = new StringBuilder();
+            for (int i = 0; i < countOfNumbers; i++) {
+                if (visited[i]) {
+                    System.out.print(lotto[i] + " ");
+                    //answer.append(lotto[i]).append(" ");
+                }
+            }
+            System.out.println();
+            //System.out.println(answer);
+        }
+        for (int i = idx; i < countOfNumbers; i++) {
+            visited[i] = true;
+            dfs(lotto, i + 1, count + 1);
+            visited[i] = false;
+        }
+    }
 }
