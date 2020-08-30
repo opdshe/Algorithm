fd63d369f35e520d7bc224b48c4e22db8ddc82f6
opdshe <opdshe@naver.com>
2020-08-30 17:06:22 +0900

    Add : 설탕배달

---
+++ java-workspace2/src/백준/다이나믹프로그래밍/설탕배달.java
@@ -0,0 +1,22 @@
+package 백준.다이나믹프로그래밍;
+
+import java.util.Scanner;
+
+public class 설탕배달 {
+	static int target;
+	static int ans = -1;
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		target = scanner.nextInt();
+		int maxFive = target / 5;
+		for (int i = maxFive; i >= 0; i--) {
+			if ((target - (i * 5)) % 3 != 0) {
+				continue;
+			}
+			ans = i + (target - (i * 5)) / 3;
+			break;
+		}
+		System.out.println(ans);
+	}
+}
