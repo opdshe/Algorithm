7bd8922f61ba850e60c063222525e7e7452fd0e4
opdshe <opdshe@naver.com>
2020-09-01 16:45:06 +0900

    Add : 기둥과보 (미완성)

--- java-workspace2/src/카카오상반기2020/기둥과보.java
+++ java-workspace2/src/카카오상반기2020/기둥과보.java
@@ -1,99 +1,108 @@
 package 카카오상반기2020;
 
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class 기둥과보 {
 	static int N;
 	static boolean[][] columns;
 	static boolean[][] carpets;
+	static List<int[]> answer = new ArrayList<>();
 
 	public static void main(String[] args) {
-		solution(5, new int[][]{{1, 0, 0, 1}, {1, 1, 1, 1}, {2, 1, 0, 1}, {2, 2, 1, 1}, {5, 0, 0, 1}, {5, 1, 0, 1}, {4, 2, 1, 1}, {3, 2, 1, 1}});
+		solution(5, new int[][]
+				{{0, 0, 0, 1}, {2, 0, 0, 1}, {4, 0, 0, 1}, {0, 1, 1, 1}, {1, 1, 1, 1},
+						{2, 1, 1, 1}, {3, 1, 1, 1}, {2, 0, 0, 0}, {1, 1, 1, 0}, {2, 2, 0, 1}}
+		);
 	}
 
 	public static int[][] solution(int n, int[][] build_frame) {
-		N = n;
-		columns = new boolean[N + 1][N + 1];
-		carpets = new boolean[N + 1][N + 1];
-		for (int[] info : build_frame) {
-			int[] position = convertKakaoToOrigin(info[0], info[1]);
-			operate(position, info[2], info[3]);
+		int size = n;
+		columns = new boolean[size + 3][size + 3];
+		carpets = new boolean[size + 3][size + 3];
+		for (int[] order : build_frame) {
+			order[0]++;
+			order[1]++;
+			operate(order);
 		}
-		for (int i = 0; i < columns.length; i++) {
-			System.out.println(Arrays.toString(columns[i]));
+
+		for (int i = 0; i <= size + 2; i++) {
+			for (int j = 0; j <= size + 2; j++) {
+				if (columns[j][i]) {
+					answer.add(new int[]{i - 1, j - 1, 0});
+				}
+				if (carpets[j][i]) {
+					answer.add(new int[]{i - 1, j - 1, 1});
+				}
+			}
 		}
-		System.out.println();
-		for (int i = 0; i < columns.length; i++) {
-			System.out.println(Arrays.toString(carpets[i]));
+		for (int[] ints : answer) {
+			System.out.println(Arrays.toString(ints));
 		}
-		return null;
+		return answer.toArray(new int[answer.size()][]);
 	}
 
-	private static void operate(int[] position, int target, int order) {
-		//target 0 = 기둥 , 1 = 보
-		if (target == 0) {
-			if (order == 1) {
-				createColumn(position);
+	private static void operate(int[] order) {
+		if (order[3] == 0) {
+			//기둥삭제
+			if (order[2] == 0) {
+				deleteColumn(order);
 			} else {
-				deleteColumn(position);
+				deleteCarpet(order);
 			}
 		} else {
-			if (order == 1) {
-				createCarpet(position);
+			//기둥설치
+			if (order[2] == 0) {
+				createColumn(order);
 			} else {
-				deleteCarpet(position);
+				createCarpet(order);
 			}
 		}
 	}
 
-	private static void deleteCarpet(int[] position) {
-		if (checkColumn(new int[]{position[0], position[1]}) &&
-				checkColumn(new int[]{position[0], position[1] + 1}) &&
-				checkCarpet(new int[]{position[0], position[1] - 1}) &&
-				checkCarpet(new int[]{position[0], position[1] + 1})) {
-			carpets[position[0]][position[1]] = false;
+	private static void deleteColumn(int[] order) {
+		if (!columns[order[1]][order[0]]) {
+			return;
+		}
+		columns[order[1]][order[0]] = false;
+		if (!(isAvailableColumn(order[1] + 1, order[0]) &&
+				isAvailableCarpet(order[1], order[0] - 1) && isAvailableCarpet(order[1], order[0]))) {
+			columns[order[1]][order[0]] = true;
 		}
 	}
 
-	private static void deleteColumn(int[] position) {
-		if (checkColumn(new int[]{position[0] - 1, position[1]}) &&
-				checkCarpet(new int[]{position[0] + 1, position[1] - 1}) &&
-				checkCarpet(new int[]{position[0] + 1, position[1]})
-		) {
-			columns[position[0]][position[1]] = false;
+	private static void deleteCarpet(int[] order) {
+		if (!carpets[order[1]][order[0]]) {
+			return;
+		}
+		carpets[order[1]][order[0]] = false;
+		if (!(isAvailableCarpet(order[1], order[0] - 1) && isAvailableCarpet(order[1], order[0] + 1) &&
+				isAvailableColumn(order[1], order[0]) && isAvailableColumn(order[1], order[0] + 1))) {
+			carpets[order[1]][order[0]] = true;
 		}
 	}
 
-
-	private static void createCarpet(int[] position) {
-		if (checkCarpet(position)) {
-			System.out.println("crate cartet " + Arrays.toString(position));
-			carpets[position[0]][position[1]] = true;
+	private static void createColumn(int[] order) {
+		if (isAvailableColumn(order[1], order[0])) {
+			columns[order[1]][order[0]] = true;
 		}
 	}
 
-	private static void createColumn(int[] position) {
-		if (checkColumn(position)) {
-			System.out.println("crate column " + Arrays.toString(position));
-			columns[position[0]][position[1]] = true;
+	private static void createCarpet(int[] order) {
+		if (isAvailableCarpet(order[1], order[0])) {
+			carpets[order[1]][order[0]] = true;
 		}
 	}
 
-	private static boolean checkCarpet(int[] position) {
-		return columns[position[0]][position[1]] || columns[position[0]][position[1] + 1] ||
-				carpets[position[0]][position[1] - 1] && carpets[position[0]][position[1] + 1];
+	private static boolean isAvailableColumn(int height, int width) {
+		return height == 1 || carpets[height][width - 1] || carpets[height][width] ||
+				columns[height - 1][width];
 	}
 
-	private static boolean checkColumn(int[] position) {
-		return position[0] == N || carpets[position[0]][position[1] - 1] ||
-				columns[position[0]][position[1] + 1];
-	}
-
-	private static int[] convertKakaoToOrigin(int x, int y) {
-		return new int[]{N - y, x};
-	}
-
-	private static int[] convertOriginToKakao(int[] position) {
-		return new int[]{position[1], N - position[0]};
+	private static boolean isAvailableCarpet(int height, int width) {
+		return columns[height - 1][width] || columns[height - 1][width + 1] ||
+				(carpets[height][width - 1] && carpets[height][width + 1]);
 	}
 }
