f127e91efb972af2d5f7c670eea8f39c5a043e11
opdshe <opdshe@naver.com>
2020-09-08 12:45:21 +0900

    Add : 이진검색트리

---
+++ java-workspace2/src/백준/재귀/이진검색트리.java
@@ -0,0 +1,68 @@
+package 백준.재귀;
+
+import java.util.Scanner;
+
+public class 이진검색트리 {
+	static Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		Tree tree = new Tree();
+		while (scanner.hasNext()) {
+			try {
+				int value = scanner.nextInt();
+				tree.add(new Node(value));
+			} catch (Exception e) {
+				break;
+			}
+
+		}
+		tree.printPostOrder(tree.root);
+	}
+
+	private static class Tree {
+		Node root;
+
+		private void printPostOrder(Node node) {
+			if (node == null) {
+				return;
+			}
+			printPostOrder(node.left);
+			printPostOrder(node.right);
+			System.out.println(node.value);
+		}
+
+		private void add(Node node) {
+			if (root == null) {
+				root = node;
+			} else {
+				add(node, root);
+			}
+		}
+
+		private void add(Node node, Node root) {
+			if (node.value < root.value) {
+				if (root.left == null) {
+					root.left = node;
+				} else {
+					add(node, root.left);
+				}
+			} else {
+				if (root.right == null) {
+					root.right = node;
+				} else {
+					add(node, root.right);
+				}
+			}
+		}
+	}
+
+	private static class Node {
+		private int value;
+		private Node left;
+		private Node right;
+
+		public Node(int value) {
+			this.value = value;
+		}
+	}
+}
--- java-workspace2/src/코테대비중요문제/산만한고양이.java
+++ java-workspace2/src/코테대비중요문제/산만한고양이.java
@@ -7,10 +7,13 @@ public class 산만한고양이 {
 	static int N;
 	static int M;
 	static Map<Integer, Set<Integer>> map = new HashMap<>();
+	static boolean[] visited;
+	static int answer = 0;
 
 	public static void main(String[] args) {
 		N = scanner.nextInt();
 		M = scanner.nextInt();
+		visited = new boolean[N + 1];
 		for (int i = 0; i < M; i++) {
 			int source = scanner.nextInt();
 			int dest = scanner.nextInt();
@@ -21,6 +24,34 @@ public class 산만한고양이 {
 			sourceMap.add(dest);
 			map.put(source, sourceMap);
 		}
-		System.out.println(Arrays.toString(map.get(2).toArray()));
+		for (int i = 1; i <= N; i++) {
+			if (i == N) {
+				if (!hasCycle(1, -1, i)) {
+					answer += i;
+				}
+			} else {
+				if (!hasCycle(i + 1, -1, i)) {
+					answer += i;
+				}
+			}
+			visited = new boolean[N + 1];
+		}
+		System.out.println(answer);
+	}
+
+	private static boolean hasCycle(int current, int parent, int except) {
+		visited[current] = true;
+		for (Integer next : map.get(current)) {
+			if (next != except) {
+				if (!visited[next]) {
+					if (hasCycle(next, current, except)) {
+						return true;
+					}
+				} else if (next != parent) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 }
