ba09eb1f328d9f9fdb9a968e0396bb8ee0dee182
opdshe <opdshe@naver.com>
2020-09-19 16:33:10 +0900

    Add : 큰수의법칙

---
+++ java-workspace2/src/백준/그리디/큰수의법칙.java
@@ -0,0 +1,30 @@
+package 백준.그리디;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class 큰수의법칙 {
+	static Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		int N = scanner.nextInt();
+		int M = scanner.nextInt();
+		int K = scanner.nextInt();
+		int[] numbers = new int[N];
+		for (int i = 0; i < N; i++) {
+			numbers[i] = scanner.nextInt();
+		}
+		Arrays.sort(numbers);
+		int maxValue = numbers[numbers.length - 1];
+		int secondValue = numbers[numbers.length - 2];
+
+		int count = (M / (K + 1)) * K;
+		count += M % (K + 1);
+
+		int total = 0;
+		total += count * maxValue;
+		total += secondValue * (M - count);
+		System.out.println(total);
+	}
+
+}
