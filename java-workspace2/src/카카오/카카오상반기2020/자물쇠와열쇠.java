f389ee9c7ea7e923652ff99b865762bb28ad49b7
opdshe <opdshe@naver.com>
2020-09-23 21:20:55 +0900

    Add : 자물쇠와열쇠

--- java-workspace2/src/카카오/카카오상반기2020/자물쇠와열쇠.java
+++ java-workspace2/src/카카오/카카오상반기2020/자물쇠와열쇠.java
@@ -1,59 +1,82 @@
 package 카카오.카카오상반기2020;
 
-import java.util.Arrays;
-import java.util.Scanner;
-
 public class 자물쇠와열쇠 {
-	static Scanner scanner = new Scanner(System.in);
 	static int keySize;
 	static int lockSize;
-	static int[][] map;
 
 	public static void main(String[] args) {
-		solution(new int[][]{{0, 0, 0}, {1, 0, 0}, {0, 1, 1}},
-				new int[][]{{1, 1, 1}, {1, 1, 0}, {1, 0, 1}});
+		solution(new int[][]{{0, 0, 0}, {1, 0, 0}, {0, 1, 1}}, new int[][]{{1, 1, 1}, {1, 1, 0}, {1, 0, 1}});
 	}
 
 	public static boolean solution(int[][] key, int[][] lock) {
 		keySize = key.length;
 		lockSize = lock.length;
-		map = new int[2 * (keySize - 1) + lockSize][2 * (keySize - 1) + lockSize];
-		initLock(lock);
-		for (int i = 0; i < lock.length; i++) {
-			System.out.println(Arrays.toString(lock[i]));
+		int[][] map = new int[keySize * 2 + lockSize][keySize * 2 + lockSize];
+		for (int height = 0; height < lockSize; height++) {
+			for (int width = 0; width < lockSize; width++) {
+				map[height + keySize][width + keySize] = lock[height][width];
+			}
 		}
-		int[][] rotatedLock = rotate(lock);
-		for (int i = 0; i < lock.length; i++) {
-			System.out.println(Arrays.toString(rotatedLock[i]));
+		boolean result = search(map, key);
+		System.out.println(result);
+		return result;
+	}
+
+	private static boolean search(int[][] map, int[][] key) {
+		int count = 0;
+		int[][] rotatedKey = deepCopy(key);
+		while (count < 4) {
+			for (int heightStart = 0; heightStart < map.length - key.length; heightStart++) {
+				for (int widthStart = 0; widthStart < map[0].length - key.length; widthStart++) {
+					int[][] cloneMap = deepCopy(map);
+					for (int keyHeight = 0; keyHeight < key.length; keyHeight++) {
+						for (int keyWidth = 0; keyWidth < key.length; keyWidth++) {
+							cloneMap[heightStart + keyHeight][widthStart + keyWidth] += rotatedKey[keyHeight][keyWidth];
+						}
+					}
+
+					if (check(cloneMap)) {
+						return true;
+					}
+				}
+			}
+			rotatedKey = rotate(rotatedKey);
+			count++;
+		}
+		return false;
+	}
+
+	private static boolean check(int[][] map) {
+		for (int height = keySize; height < keySize + lockSize; height++) {
+			for (int width = keySize; width < keySize + lockSize; width++) {
+				if (map[height][width] != 1) {
+					return false;
+				}
+			}
 		}
 		return true;
 	}
 
-	private static void initLock(int[][] lock) {
-		int startHeight = keySize - 1;
-		for (int i = 0; i < lock.length; i++) {
-			for (int j = 0; j < lock.length; j++) {
-				map[i + startHeight][j + startHeight] = lock[i][j];
+	static int[][] rotate(int[][] arr) {
+		int n = arr.length;
+		int m = arr[0].length;
+		int[][] rotate = new int[m][n];
+
+		for (int i = 0; i < rotate.length; i++) {
+			for (int j = 0; j < rotate[i].length; j++) {
+				rotate[i][j] = arr[n - 1 - j][i];
 			}
 		}
+		return rotate;
 	}
 
-	private static int[][] rotate(int[][] target) {
-		int[][] rotatedTarget = new int[target.length][target.length];
-		for (int i = 0; i < target.length; i++) {
-			for (int j = 0; j < target.length; j++) {
-				rotatedTarget[j][target.length - 1 - i] = target[i][j];
-			}
-		}
-		return rotatedTarget;
-	}
+	private static int[][] deepCopy(int[][] origin) {
+		if (origin == null) return null;
+		int[][] result = new int[origin.length][origin[0].length];
 
-	private static boolean isAvailableToUnlock(int startHeight, int startWidth, int[][] key) {
-		for (int h = startHeight; h < keySize; h++) {
-			for (int w = startWidth; w < keySize; w++) {
-				map[h][w] = key[h - startHeight][w - startWidth];
-			}
+		for (int i = 0; i < origin.length; i++) {
+			System.arraycopy(origin[i], 0, result[i], 0, origin[0].length);
 		}
-		return true;
+		return result;
 	}
 }
