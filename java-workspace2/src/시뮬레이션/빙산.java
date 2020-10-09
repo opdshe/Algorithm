2d0f939656db4be2d4163fd5ac13e25a7f32a6a3
opdshe <opdshe@naver.com>
2020-10-09 15:13:27 +0900

    Add : 빙산

--- java-workspace2/src/시뮬레이션/빙산.java
+++ java-workspace2/src/시뮬레이션/빙산.java
@@ -1,16 +1,16 @@
 package 시뮬레이션;
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Queue;
 
 public class 빙산 {
 	static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-	static int maxRow;
-	static int maxColumn;
-	static int[][] map;
 	static List<int[]> directions = Arrays.asList(
 			new int[]{-1, 0},
 			new int[]{1, 0},
@@ -22,37 +22,96 @@ public class 빙산 {
 		int[] input = Arrays.stream(bufferedReader.readLine().split(" "))
 				.mapToInt(Integer::parseInt)
 				.toArray();
-		maxRow = input[0];
-		maxColumn = input[1];
-		map = new int[maxRow][maxColumn];
-		for (int row = 0; row < maxRow; row++) {
-			int[] rowInfo = Arrays.stream(bufferedReader.readLine().split(" "))
+		int[][] map = new int[input[0]][input[1]];
+		for (int row = 0; row < input[0]; row++) {
+			map[row] = Arrays.stream(bufferedReader.readLine().split(" "))
 					.mapToInt(Integer::parseInt)
 					.toArray();
+		}
+		solution(map, input[0], input[1]);
+	}
+
+	private static void solution(int[][] map, int maxRow, int maxColumn) {
+		int time = 0;
+		while (isOneChunk(map, maxRow, maxColumn)) {
+			int[][] removeMap = new int[maxRow][maxColumn];
+			for (int row = 0; row < maxRow; row++) {
+				for (int column = 0; column < maxColumn; column++) {
+					int count = 0;
+					for (int[] direction : directions) {
+						int nextRow = row + direction[0];
+						int nextColumn = column + direction[1];
+						if (isAvailablePosition(maxRow, maxColumn, nextRow, nextColumn)) {
+							if (map[nextRow][nextColumn] == 0) {
+								count++;
+							}
+						}
+					}
+					removeMap[row][column] = count;
+				}
+			}
+
+			int remain = 0;
+			for (int row = 0; row < maxRow; row++) {
+				for (int column = 0; column < maxColumn; column++) {
+					map[row][column] -= removeMap[row][column];
+					if (map[row][column] > 0) {
+						remain++;
+					} else {
+						map[row][column] = 0;
+					}
+				}
+			}
+			if (remain == 0) {
+				time = 0;
+				break;
+			}
+			time++;
+		}
+		System.out.println(time);
+	}
+
+	private static boolean isOneChunk(int[][] map, int maxRow, int maxColumn) {
+		boolean[][] visited = new boolean[maxRow][maxColumn];
+		int count = 0;
+		boolean result = true;
+		for (int row = 0; row < maxRow; row++) {
 			for (int column = 0; column < maxColumn; column++) {
-				map[row][column] = rowInfo[column];
+				if (!visited[row][column] && map[row][column] > 0) {
+					if (count > 0) {
+						result = false;
+						break;
+					}
+					connect(map, visited, maxRow, maxColumn, row, column);
+					count++;
+				}
 			}
 		}
-		solution();
+		return result;
 	}
 
-	private static void solution() {
-		int time = 0;
-		while (true) {
+	private static void connect(int[][] map, boolean[][] visited, int maxRow, int maxColumn, int row, int column) {
+		Queue<int[]> queue = new ArrayDeque<>();
+		visited[row][column] = true;
+		queue.add(new int[]{row, column});
 
+		while (!queue.isEmpty()) {
+			int[] current = queue.poll();
+			for (int[] direction : directions) {
+				int nextRow = current[0] + direction[0];
+				int nextColumn = current[1] + direction[1];
+				if (isAvailablePosition(maxRow, maxColumn, nextRow, nextColumn)) {
+					if (!visited[nextRow][nextColumn] && map[nextRow][nextColumn] > 0) {
+						visited[nextRow][nextColumn] = true;
+						queue.add(new int[]{nextRow, nextColumn});
+					}
+				}
+			}
 		}
 	}
 
 
-	private static boolean isOneChunk(int row, int column) {
-		return false;
-	}
-
-	private static void setRemoveSize() {
-
-	}
-
-	private static boolean isAvailablePosition(int nextRow, int nextColumn) {
+	private static boolean isAvailablePosition(int maxRow, int maxColumn, int nextRow, int nextColumn) {
 		return nextRow >= 0 && nextRow < maxRow &&
 				nextColumn >= 0 && nextColumn < maxColumn;
 	}
