303fe7c464ef212fb366471c3fb6ecb23fbbc751
opdshe <opdshe@naver.com>
2020-10-12 18:34:44 +0900

    Add : 피보나치수5

---
+++ java-workspace2/src/다이나믹프로그래밍/피보나치수5.java
@@ -0,0 +1,26 @@
+package 다이나믹프로그래밍;
+
+import java.util.Scanner;
+
+public class 피보나치수5 {
+	static Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		int target = scanner.nextInt();
+		int[] dp = new int[target + 1];
+		System.out.println(fib(dp, target));
+	}
+
+	private static int fib(int[] dp, int target) {
+		if (dp[target] != 0) {
+			return dp[target];
+		}
+		if (target == 0) {
+			return 0;
+		}
+		if (target == 1) {
+			return 1;
+		}
+		return dp[target] = fib(dp, target - 1) + fib(dp, target - 2);
+	}
+}
