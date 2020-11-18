7819ee44d551d03ae827390004e0945d89ac549a
opdshe <opdshe@naver.com>
2020-11-18 20:43:26 +0900

    Add : 벽부수고 이동하기 (BFS)

--- java-workspace2/src/복습/벽부수고이동하기.java
+++ java-workspace2/src/복습/벽부수고이동하기.java
@@ -1,8 +1,6 @@
 package 복습;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class 벽부수고이동하기 {
 	static Scanner scanner = new Scanner(System.in);
@@ -15,7 +13,6 @@ public class 벽부수고이동하기 {
 			new int[]{0, -1},
 			new int[]{0, 1}
 	);
-	static int answer = Integer.MAX_VALUE;
 
 	public static void main(String[] args) {
 		maxRow = scanner.nextInt();
@@ -27,33 +24,39 @@ public class 벽부수고이동하기 {
 					.mapToInt(Integer::parseInt)
 					.toArray();
 		}
-		boolean[][] visited = new boolean[maxRow][maxColumn];
-		dfs(visited, 0, 0, 1, false);
-		System.out.println(answer == Integer.MAX_VALUE ? -1 : answer);
+		solution();
 	}
 
-	private static void dfs(boolean[][] visited, int row, int column, int cost, boolean destroy) {
-		if (row == maxRow - 1 && column == maxColumn - 1) {
-			answer = Math.min(answer, cost);
-			return;
-		}
-		for (int[] direction : directions) {
-			int nextRow = row + direction[0];
-			int nextColumn = column + direction[1];
-			if (isAvailablePosition(nextRow, nextColumn)) {
-				if (map[nextRow][nextColumn] == 0 && !visited[nextRow][nextColumn]) {
-					visited[nextRow][nextColumn] = true;
-					dfs(visited, nextRow, nextColumn, cost + 1, destroy);
-					visited[nextRow][nextColumn] = false;
-				} else if (map[nextRow][nextColumn] == 1 && !visited[nextRow][nextColumn]) {
-					if (!destroy) {
-						visited[nextRow][nextColumn] = true;
-						dfs(visited, nextRow, nextColumn, cost + 1, true);
-						visited[nextRow][nextColumn] = false;
+	private static void solution() {
+		boolean[][][] visited = new boolean[maxRow][maxColumn][2];
+		Queue<Node> queue = new ArrayDeque<>();
+		queue.add(new Node(0, 0, 1, 0));
+		visited[0][0][0] = true;
+
+		int answer = -1;
+		while (!queue.isEmpty()) {
+			Node current = queue.poll();
+			if (current.y == maxRow - 1 && current.x == maxColumn - 1) {
+				answer = current.cost;
+				break;
+			}
+			for (int[] direction : directions) {
+				int nextRow = current.y + direction[0];
+				int nextColumn = current.x + direction[1];
+				if (isAvailablePosition(nextRow, nextColumn)) {
+					if (map[nextRow][nextColumn] == 0 && !visited[nextRow][nextColumn][current.destroy]) {
+						visited[nextRow][nextColumn][current.destroy] = true;
+						queue.add(new Node(nextRow, nextColumn, current.cost + 1, current.destroy));
+					} else if (map[nextRow][nextColumn] == 1) {
+						if (current.destroy == 0 && !visited[nextRow][nextColumn][1]) {
+							visited[nextRow][nextColumn][1] = true;
+							queue.add(new Node(nextRow, nextColumn, current.cost + 1, 1));
+						}
 					}
 				}
 			}
 		}
+		System.out.println(answer);
 	}
 
 	private static boolean isAvailablePosition(int nextRow, int nextColumn) {
@@ -65,9 +68,9 @@ public class 벽부수고이동하기 {
 		int y;
 		int x;
 		int cost;
-		boolean destroy;
+		int destroy;
 
-		public Node(int y, int x, int cost, boolean destroy) {
+		public Node(int y, int x, int cost, int destroy) {
 			this.y = y;
 			this.x = x;
 			this.cost = cost;
