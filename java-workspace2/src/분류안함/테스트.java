b7db2f3c7a00ca2a289247b57058e18ffdee2302
opdshe <opdshe@naver.com>
2020-11-25 15:54:06 +0900

    feat: 삼번 기능 구현

--- java-workspace2/src/분류안함/테스트.java
+++ java-workspace2/src/분류안함/테스트.java
@@ -2,9 +2,10 @@ package 분류안함;
 
 public class 테스트 {
 
-	public static void main(String[] args) {
-		System.out.println("test commit 1");
-		System.out.println("test commit 2");
-	}
+    public static void main(String[] args) {
+        System.out.println("test commit 1");
+        System.out.println("test commit 2");
+        System.out.println("test commit 3");
+    }
 
 }
