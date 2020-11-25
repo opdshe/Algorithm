8e84f795997eb8be81cbf38e31146455783415a8
opdshe <opdshe@naver.com>
2020-11-25 15:47:42 +0900

    feat: 일번기능 구현

--- java-workspace2/src/분류안함/테스트.java
+++ java-workspace2/src/분류안함/테스트.java
@@ -1,21 +1,9 @@
 package 분류안함;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class 테스트 {
 
 	public static void main(String[] args) {
-		Set<Set<Integer>> set = new HashSet<>();
-		Set<Integer> a = new HashSet<>();
-		Set<Integer> b = new HashSet<>();
-		a.add(1);
-		a.add(2);
-		b.add(1);
-		b.add(3);
-		set.add(a);
-		System.out.println(set.contains(b));
-
+		System.out.println("test commit 1");
 	}
 
 }
