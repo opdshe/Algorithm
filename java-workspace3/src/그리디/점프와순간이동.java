aa4275689d733a1f8e0e69c236f729db81147730
opdshe <opdshe@naver.com>
2021-01-04 20:22:07 +0900

    Add: 점프와 순간이동

---
+++ java-workspace3/src/그리디/점프와순간이동.java
@@ -0,0 +1,21 @@
+package 그리디;
+
+public class 점프와순간이동 {
+    public static void main(String[] args) {
+        solution(5);
+    }
+
+    public static int solution(int n) {
+        int ans = 0;
+        while (n != 0) {
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                ans++;
+                n--;
+            }
+        }
+        System.out.println(ans);
+        return ans;
+    }
+}
