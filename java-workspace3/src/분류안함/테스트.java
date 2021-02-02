30415f1729ae152ad65d6790e5b1f63bbd2309b3
opdshe <opdshe@naver.com>
2021-02-02 17:32:03 +0900

    Add: 이분탐색 라이브러리 추가

--- java-workspace3/src/분류안함/테스트.java
+++ java-workspace3/src/분류안함/테스트.java
@@ -1,32 +1,11 @@
 package 분류안함;
 
-import java.util.*;
-import java.util.stream.Collectors;
-
 public class 테스트 {
     public static void main(String[] args) {
-        List<Music> musics = new ArrayList<>();
-        musics.add(new Music(0, 500, "rock"));
-        musics.add(new Music(1, 100, "hiphop"));
-        musics.add(new Music(2, 200, "rock"));
-        musics.add(new Music(3, 400, "hiphop"));
-        musics.add(new Music(4, 600, "rock"));
-        musics.add(new Music(5, 100, "ballad"));
-
-        Map<String, List<Music>> group = musics.stream()
-                .collect(Collectors.groupingBy(music -> music.genre));
-        Set<Character> sets = new HashSet<>();
+        String str = "하이";
     }
 
-    private static class Music {
-        int idx;
-        int play;
-        String genre;
-
-        public Music(int idx, int play, String genre) {
-            this.idx = idx;
-            this.play = play;
-            this.genre = genre;
-        }
+    private static void convert(String origin) {
+        origin = "hello";
     }
 }
