0469589a3c0c7555bb368a17879a30249bec4cee
opdshe <opdshe@naver.com>
2021-02-04 15:50:50 +0900

    Add: 숨바꼭질 2, 3

---
+++ java-workspace3/src/깊이우선탐색/숨바꼭질2.java
@@ -0,0 +1,59 @@
+package 깊이우선탐색;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class 숨바꼭질2 {
+	static Scanner scanner = new Scanner(System.in);
+	static int INF = Integer.MAX_VALUE;
+
+	public static void main(String[] args) {
+		int subin = scanner.nextInt();
+		int sister = scanner.nextInt();
+		solution(subin, sister);
+	}
+
+	private static void solution(int subin, int sister) {
+		int count = 0;
+		int minTime = INF;
+		Queue<Node> queue = new ArrayDeque<>();
+		int[] times = new int[100001];
+		Arrays.fill(times, INF);
+		times[subin] = 0;
+
+		queue.add(new Node(subin, 0));
+		while (!queue.isEmpty()) {
+			Node current = queue.poll();
+			if (current.position == sister) {
+				minTime = Math.min(minTime, current.time);
+				count++;
+			}
+			if (current.position - 1 >= 0 && times[current.position - 1] >= current.time + 1) {
+				times[current.position - 1] = current.time + 1;
+				queue.add(new Node(current.position - 1, current.time + 1));
+			}
+			if (current.position + 1 <= 100000 && times[current.position + 1] >= current.time + 1) {
+				times[current.position + 1] = current.time + 1;
+				queue.add(new Node(current.position + 1, current.time + 1));
+			}
+			if (current.position * 2 <= 100000 && times[current.position * 2] >= current.time + 1) {
+				times[current.position * 2] = current.time + 1;
+				queue.add(new Node(current.position * 2, current.time + 1));
+			}
+		}
+		System.out.println(minTime);
+		System.out.println(count);
+	}
+
+	private static class Node {
+		int position;
+		int time;
+
+		public Node(int position, int time) {
+			this.position = position;
+			this.time = time;
+		}
+	}
+}
---
+++ java-workspace3/src/깊이우선탐색/숨바꼭질3.java
@@ -0,0 +1,56 @@
+package 깊이우선탐색;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class 숨바꼭질3 {
+	static Scanner scanner = new Scanner(System.in);
+	static int INF = Integer.MAX_VALUE;
+
+	public static void main(String[] args) {
+		int subin = scanner.nextInt();
+		int sister = scanner.nextInt();
+		solution(subin, sister);
+	}
+
+	private static void solution(int subin, int sister) {
+		int minTime = INF;
+		Queue<Node> queue = new ArrayDeque<>();
+		int[] times = new int[100001];
+		Arrays.fill(times, INF);
+		times[subin] = 0;
+
+		queue.add(new Node(subin, 0));
+		while (!queue.isEmpty()) {
+			Node current = queue.poll();
+			if (current.position == sister) {
+				minTime = Math.min(minTime, current.time);
+			}
+			if (current.position - 1 >= 0 && times[current.position - 1] > current.time + 1) {
+				times[current.position - 1] = current.time + 1;
+				queue.add(new Node(current.position - 1, current.time + 1));
+			}
+			if (current.position + 1 <= 100000 && times[current.position + 1] > current.time + 1) {
+				times[current.position + 1] = current.time + 1;
+				queue.add(new Node(current.position + 1, current.time + 1));
+			}
+			if (current.position * 2 <= 100000 && times[current.position * 2] > current.time) {
+				times[current.position * 2] = current.time;
+				queue.add(new Node(current.position * 2, current.time));
+			}
+		}
+		System.out.println(minTime);
+	}
+
+	private static class Node {
+		int position;
+		int time;
+
+		public Node(int position, int time) {
+			this.position = position;
+			this.time = time;
+		}
+	}
+}
